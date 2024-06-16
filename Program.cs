5599da2ac6ddbe45a30599fe54894ec14c1f1529
Hyk_Jn <gurwls1594@gmail.com>
2024-06-17 04:47:09 +0900

    Stack
    Data Structure 0: Stack
    1.0

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            StackEX();
+        }
+
+        public static void StackEX()
+        {
+            Stack<int> myStack = new(5);
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.PrintContainer();
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public struct Stack<T>
+{
+    public int size;
+    public bool IsEmpty
+    {
+        get
+        {
+            return top == 0;
+        }
+    }
+    public bool IsFull
+    {
+        get
+        {
+            return top == size;
+        }
+    }
+
+    int top;
+    T?[] container;
+    public void Push(T value)
+    {
+        if (IsFull)
+        {
+            Console.WriteLine("Can't Push Because Stack is Full");
+        }
+        else container[top++] = value;
+    }
+
+    public T? Pop()
+    {
+        if (IsEmpty)
+        {
+            Console.WriteLine("Empty");
+            return default;
+        }
+        else
+        {
+            T? temp = container[--top];
+            container[top] = default;
+            return temp;
+        }
+    }
+
+    public T? Peek()
+    {
+        if (IsEmpty)
+        {
+            Console.WriteLine("Empty");
+            return default;
+        }
+        else
+        {
+            return container[top - 1];
+        }
+    }
+    
+    public void PrintContainer()
+    {
+        for (int i = size - 1; i >= 0; i--)
+        {
+            if (i + 1 == top) Console.WriteLine($"|{i}|{container[i]}" + " <--Top");
+            else Console.WriteLine($"|{i}|{container[i]}");
+        }
+    }
+
+    public Stack(int size)
+    {
+        this.size = size;
+        container = new T[this.size];
+        top = 0;
+    }
+}
