e830e13cc02f11d094096e079f0140f3ebc9b86a
Hyk_Jn <gurwls1594@gmail.com>
2024-06-26 19:16:40 +0900

    Add Deque

--- Program.cs
+++ Program.cs
@@ -7,6 +7,29 @@ namespace Program
     {
         public static void Main()
         {
+
+        }
+
+        public static void StackEX()
+        {
+            MyStack<int> myStack = new(5);
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+            myStack.PrintContainer();
+        }
+
+        public static void LinearQueueEX()
+        {
+            MyLinearQueue<int> myQueue = new(5);
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+            myQueue.PrintContainer();
+        }
+
+        public static void CircularQueueEX()
+        {
             MyCircularQueue<int> myQueue = new(5);
             myQueue.Enqueue(1);
             myQueue.Enqueue(2);
@@ -19,22 +42,16 @@ namespace Program
             myQueue.PrintContainer();
         }
 
-        public static void StackEX()
+        public static void DequeEX()
         {
-            MyStack<int> myStack = new(5);
-            myStack.Push(1);
-            myStack.Push(2);
-            myStack.Push(3);
-            myStack.PrintContainer();
-        }
-
-        public static void QueueEX()
-        {
-            MyLinearQueue<int> myQueue = new(5);
-            myQueue.Enqueue(1);
-            myQueue.Enqueue(2);
-            myQueue.Enqueue(3);
-            myQueue.PrintContainer();
+            MyDeque<int> myDeque = new(5);
+            myDeque.AddFront(1);
+            myDeque.AddRear(2);
+            myDeque.AddFront(3);
+            myDeque.AddRear(4);
+            Console.WriteLine(myDeque.PopFront());
+            Console.WriteLine(myDeque.PopFront());
+            myDeque.PrintContainer();
         }
     }
 }
--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -179,4 +179,147 @@
             this.count = 0;
         }
     }
+
+    public struct MyDeque<T>
+    {
+        public bool IsFull
+        {
+            get
+            {
+                return count == size;
+            }
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                return count == 0;
+            }
+        }
+        public int size;
+
+        int front, rear, count;
+        T[] container;
+
+        public void AddFront(T item)
+        {
+            if (IsFull)
+            {
+                Console.WriteLine("Can't add to front because deque is full");
+            }
+            else
+            {
+                front = MoveIndex(front, -1);
+                container[front] = item;
+                count++;
+            }
+        }
+
+        public void AddRear(T item)
+        {
+            if (IsFull)
+            {
+                Console.WriteLine("Can't add to rear because deque is full");
+            }
+            else
+            {
+                container[rear] = item;
+                count++;
+                rear = MoveIndex(rear, 1);
+            }
+        }
+
+        public T? PopFront()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("Empty");
+                return default;
+            }
+            else
+            {
+                T item = container[front];
+                container[front] = default;
+                front = MoveIndex(front, 1);
+                count--;
+                return item;
+            }
+        }
+
+        public T? PopRear()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("Empty");
+                return default;
+            }
+            else
+            {
+                container[rear] = default;
+                rear = MoveIndex(rear, -1);
+                T item = container[rear];
+                count--;
+                return item;
+            }
+        }
+
+        public T? GetFront()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("Empty");
+                return default;
+            }
+            else
+            {
+                return container[front];
+            }
+        }
+
+        public T? GetRear()
+        {
+            if (IsEmpty)
+            {
+                Console.WriteLine("Empty");
+                return default;
+            }
+            else
+            {
+                return container[MoveIndex(rear, -1)];
+            }
+        }
+
+        public void PrintContainer()
+        {
+            string front = "", index = "", item = "", rear = "";
+            for (int i = 0; i < size; i++)
+            {
+                if (!IsEmpty && i == this.front) front += "Front";
+                else front += "   ";
+                index += $"|{i}|";
+                item += $" {container[i]} ";
+                if (!IsEmpty && MoveIndex(i, 1) == this.rear) rear += "Rear";
+                else rear += "   ";
+            }
+            Console.WriteLine(front);
+            Console.WriteLine(index);
+            Console.WriteLine(item);
+            Console.WriteLine(rear);
+        }
+
+        int MoveIndex(int index, int count)
+        {
+            if (count > 0) return (index + count) % size;
+            else if (count < 0) return (size + index + count) % size;
+            else return index;
+        }
+        public MyDeque(int size)
+        {
+            this.size = size;
+            front = 0;
+            rear = 0;
+            count = 0;
+            container = new T[size];
+        }
+    }
 }
