33690c0c5ed22ec201001fcb732ab2a2ab83778f
Hyk_Jn <gurwls1594@gmail.com>
2025-06-13 16:02:50 +0900

    Commit #2
    Working on LinkedList

---
+++ Linear/LinkedList.cs
@@ -0,0 +1,76 @@
+﻿namespace CS_DataStructure.Linear
+{
+    using Nodes;
+    public class SinglyLinkedList<T> where T : notnull
+    {
+        #region ==========Fields==========
+        public int Count { get; private set; }
+        public T this[int index]
+        {
+            get;
+            set;
+        }
+
+        private LinkedNode<T>? head;
+        #endregion
+
+        #region ==========Methods==========
+        public void Insert(int index, T item)
+        {
+
+        }
+        public void AddFirst(T item) => Insert(0, item);
+        public void AddLast(T item) => Insert(Count, item);
+
+        public void RemoveAt(int index)
+        {
+
+        }
+        public void RemoveFirst() => RemoveAt(0);
+        public void RemoveLast() => RemoveAt(Count - 1);
+
+        public bool Remove(T item)
+        {
+
+        }
+
+        public T Get(int index)
+        {
+
+        }
+
+        public int IndexOf(T item)
+        {
+
+        }
+
+        public bool Contains(T item)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+        #endregion
+
+        #region ==========Constructors==========
+        public SinglyLinkedList()
+        {
+            head = null;
+            Count = 0;
+        }
+        #endregion
+    }
+
+    public class DoublyLinkedList<T> where T : notnull
+    {
+
+    }
+
+    public class CircularLinkedList<T> where T : notnull
+    {
+
+    }
+}
---
+++ Nodes/Nodes.cs
@@ -0,0 +1,18 @@
+﻿namespace CS_DataStructure.Nodes
+{
+    public class Node<T>(T value) where T : notnull
+    {
+        public T Value { get; set; } = value;
+    }
+
+    public class LinkedNode<T>(T value) : Node<T>(value) where T : notnull
+    {
+        public LinkedNode<T>? Neighbor { get; set; } = null;
+    }
+
+    public class LinkedNode2<T>(T value) : LinkedNode<T>(value) where T : notnull
+    {
+        public LinkedNode<T>? Left => Neighbor;
+        public LinkedNode<T>? Right { get; set; } = null;
+    }
+}
