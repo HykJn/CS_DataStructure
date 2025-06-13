64a5aa92da025f8a041f86134dcb94089c3682f0
Hyk_Jn <gurwls1594@gmail.com>
2025-06-13 14:25:45 +0900

    Commit #1
    Add ArrayList

---
+++ Linear/ArrayList.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+
+namespace CS_DataStructure.Linear
+{
+    public class ArrayList<T> : IList<T> where T : notnull, IEquatable<T>
+    {
+        #region ==========Fields==========  
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.count)
+                    throw new IndexOutOfRangeException();
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= this.count)
+                    throw new IndexOutOfRangeException();
+                array[index] = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get => capacity;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfNegative(value);
+                capacity = value;
+                Resize(value);
+            }
+        }
+        public int Count => count;
+        public bool IsReadOnly { get; set; }
+
+        private T[] array;
+        private int capacity;
+        private int count;
+        #endregion
+
+        #region ==========Methods==========  
+        public int IndexOf(T item)
+        {
+            //Use Linear search -> O(n)  
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Equals(item)) return i;
+            }
+            return -1;
+        }
+
+        public void Add(T item)
+        {
+            if (IsReadOnly) throw new NotSupportedException();
+            if (count == capacity) Resize();
+            array[count] = item;
+            count++;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (IsReadOnly) throw new NotSupportedException();
+            if (index < 0 || index > count) throw new IndexOutOfRangeException();
+            if (count == capacity) Resize();
+            if (index == count)
+            {
+                Add(item);
+                return;
+            }
+
+            Array.Copy(array, index, array, index + 1, count - index);
+            array[index] = item;
+            count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (IsReadOnly) throw new NotSupportedException();
+            if (index < 0 || index >= count) throw new IndexOutOfRangeException();
+            if (index == count - 1)
+            {
+                count--;
+                return;
+            }
+
+            Array.Copy(array, index + 1, array, index, count - index);
+            count--;
+        }
+
+        public bool Remove(T item)
+        {
+            if (IsReadOnly) throw new NotSupportedException();
+            int index = IndexOf(item);
+            if (index == -1) return false;
+
+            try
+            {
+                RemoveAt(index);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            if (IsReadOnly) throw new NotSupportedException();
+            capacity = 16;
+            count = 0;
+            array = new T[capacity];
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Equals(item)) return true;
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] destination, int destinationIndex)
+        {
+            if (destinationIndex < 0 || destinationIndex >= destination.Length) throw new IndexOutOfRangeException();
+            if (destination.Length - destinationIndex < count) throw new ArgumentOutOfRangeException(nameof(destinationIndex));
+
+            Array.Copy(array, 0, destination, destinationIndex, count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("==========Array List==========");
+            Console.WriteLine("Capacity: " + capacity);
+            Console.WriteLine("Count: " + count);
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"[{i}]: {array[i]}");
+            }
+        }
+
+        private void Resize(int capacity)
+        {
+            if (IsReadOnly) throw new NotSupportedException();
+            ArgumentOutOfRangeException.ThrowIfLessThan<int>(capacity, 0, nameof(capacity));
+            if (capacity == this.capacity) return;
+
+            T[] temp = new T[capacity];
+            Array.Copy(array, 0, temp, 0, count);
+            array = temp;
+            this.capacity = capacity;
+        }
+
+        private void Resize() => Resize(capacity * 2);
+        #endregion
+
+        #region ==========Constructors==========  
+        public ArrayList(int capacity)
+        {
+            this.capacity = capacity;
+            array = new T[capacity];
+            IsReadOnly = false;
+            count = 0;
+        }
+
+        public ArrayList() : this(16) { }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CS_DataStructure
+{
+    using Linear;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public static void Test_ArrayList()
+        {
+            ArrayList<int> ints = new ArrayList<int>(4) { 1, 2, 3 };
+
+            Console.WriteLine(ints.IndexOf(3)); //2
+            Console.WriteLine(ints.IndexOf(4)); //-1
+
+            ints.Add(4); //1, 2, 3, 4
+            ints.Insert(2, 5); //1, 2, 5, 3, 4 -> Capacity 4 -> 8
+
+            ints.RemoveAt(3); //1, 2, 5, 4
+            ints.Remove(1); //2, 5, 4
+
+            Console.WriteLine(ints.Contains(2));
+
+            ints.Display();
+        }
+    }
+}
